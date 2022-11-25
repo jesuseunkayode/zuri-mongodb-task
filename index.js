76e329566a73bb1b08d5f289ab325a9289321609
Kayode Owolabi Jesuseun <63752160+jesuseunkayode@users.noreply.github.com>
2022-11-25 08:53:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 const todoRouter = require('./routes/todo.js');
-
-
 app.use(todoRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`The server is up and running on port ${PORT}`);
